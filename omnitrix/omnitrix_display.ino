d854db5a272af7e7c186a9a7f2db7dea37b445ce
AA0100000101000001 <gracedgold26@gmail.com>
2023-09-18 16:30:53 +0300

    Removed Sprites

--- omnitrix/omnitrix_display.ino
+++ omnitrix/omnitrix_display.ino
@@ -24,57 +24,16 @@ void showAnimation() {
 //Method that shows the symbols for select mode
 void  ShowSelectSymbols() {
 
-  int XLup1, XLup2, YLup1, YLup2, XLdown1, XLdown2, YLdown1, YLdown2;
-  int XRup1, XRup2, YRup1, YRup2, XRdown1, XRdown2, YRdown1, YRdown2;
-  int black;
+  delay(100);
 
-  upLine.drawLine(0, 0, 40, 60, OMNITRIX_GRAY);
-  downLine.drawLine(40, 0, 0, 60, OMNITRIX_GRAY);
+  int16_t rc = png.openFLASH((uint8_t *)omnitrix_anim[9], sizeof(omnitrix_anim[9]), pngDraw);
 
-  //Yup are for the down symbols and Ydown are for the up symbols
-  //draw down and upper left symbol
-  XLup1 = -80;
-  YLup1 = 119;
-  XLup2 = -40;
-  YLup2 = 179;
-  XLdown1 = -40;
-  YLdown1 = 0;
-  XLdown2 = -80;
-  YLdown2 = 59;
-  //draw down and upper right symbol
-  XRup1 = 240;
-  YRup1 = 1;
-  XRup2 = 280;
-  YRup2 = 61;
-  XRdown1 = 280;
-  YRdown1 = 120;
-  XRdown2 = 240;
-  YRdown2 = 179;
+  if (rc == PNG_SUCCESS) {
+    Serial.println("Successfully png file");
 
-  //When XLup1 <= -5 | XLup2 <= 35 black line center is at (0, 120), add 24 to x = 59 (add 6 so black lines touch at (120, 0), (120, 240))
-  for (; /*XLup1 <= 19 | */XLup2 <= 59 ; XLup1++, XLdown1++, XLup2++, XLdown2++, XRup1--, XRdown1--, XRup2--, XRdown2--) {
-    upLine.pushSprite(XLup1, YLup1, TFT_TRANSPARENT);
-    upLine.pushSprite(XLup2, YLup2, TFT_TRANSPARENT);
-    downLine.pushSprite(XLdown1, YLdown1, TFT_TRANSPARENT);
-    downLine.pushSprite(XLdown2, YLdown2, TFT_TRANSPARENT);
-    upLine.pushSprite(XRup1, YRup1, TFT_TRANSPARENT);
-    downLine.pushSprite(XRdown1, YRdown1, TFT_TRANSPARENT);
-    upLine.pushSprite(XRup2, YRup2, TFT_TRANSPARENT);
-    downLine.pushSprite(XRdown2, YRdown2, TFT_TRANSPARENT);
-  }
-
-//draw black lines
-  upLine.drawLine(0, 0, 40, 60, TFT_BLACK);
-  downLine.drawLine(40, 0, 0, 60, TFT_BLACK);
-  for (black = 0; black <= BLACK_LINE_WIDTH; black++, XLup1++, XLdown1++, XLup2++, XLdown2++, XRup1--, XRdown1--, XRup2--, XRdown2--) {
-    upLine.pushSprite(XLup1, YLup1, TFT_TRANSPARENT);
-    upLine.pushSprite(XLup2, YLup2, TFT_TRANSPARENT);
-    downLine.pushSprite(XLdown1, YLdown1, TFT_TRANSPARENT);
-    downLine.pushSprite(XLdown2, YLdown2, TFT_TRANSPARENT);
-    upLine.pushSprite(XRup1, YRup1, TFT_TRANSPARENT);
-    downLine.pushSprite(XRdown1, YRdown1, TFT_TRANSPARENT);
-    upLine.pushSprite(XRup2, YRup2, TFT_TRANSPARENT);
-    downLine.pushSprite(XRdown2, YRdown2, TFT_TRANSPARENT);
+    tft.startWrite();
+    rc = png.decode(NULL, 0);
+    tft.endWrite();
   }
 
 }
