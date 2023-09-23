e7c365542739c770311b7edce071d4ae1b1404f8
AA0100000101000001 <gracedgold26@gmail.com>
2023-09-23 21:26:35 +0300

    Added Basic Code

--- Test files/Timer_Wake_Up_Test/Timer_Wake_Up_Test.ino
+++ Test files/Timer_Wake_Up_Test/Timer_Wake_Up_Test.ino
@@ -3,13 +3,28 @@
 #define DEEP_SLEEP_TIMER 10000 // sec
 uint32_t start; //timer for deep sleep
 
+#define ALIEN_TRANSFORMATION_TIME_TEST 5000 //5 sec
+#define OMNITRIX_RECHARGE_TIME_TEST 5000
+#define ALIEN_TRANSFORMATION_TIME_DEFAULT 60000 //1 min
+#define OMNITRIX_RECHARGE_TIME_DEFAULT 60000
+
+int transform_time_val = ALIEN_TRANSFORMATION_TIME_TEST;
+int recharge_time_val = OMNITRIX_RECHARGE_TIME_TEST;
+
+//Time variables that are saved in deep sleep mode
+RTC_DATA_ATTR uint32_t transform_timer;
+RTC_DATA_ATTR uint32_t recharge_timer;
+RTC_DATA_ATTR unsigned long millisOffset=0;
+
 RTC_DATA_ATTR int bootCount = 0;
 RTC_DATA_ATTR int mode = 1;
 
 int LED = 2;
 const int buttonPin = 5; //Start button
+const int SW = 3; //Encoder button
 
 int buttonState = 0; //State of start button
+int selectbuttonState = 0; //State of select button
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -20,12 +35,16 @@ void setup() {
   ++bootCount;
   Serial.println("Boot number: " + String(bootCount));
 
+  //The omnitrix will wake up when the button is pressed
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_5,1);
+
   //Print the wakeup reason for ESP32
   get_wakeup_reason();
 
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED, OUTPUT);
   pinMode(buttonPin, INPUT);
+  pinMode(SW, INPUT_PULLUP);
 }
 
 // the loop function runs over and over again forever
@@ -41,19 +60,49 @@ void loop() {
       digitalWrite(LED, HIGH);
       mode = 2;
       delay(200);
-      Serial.println("Button pressed");
+      Serial.println("Button pressed, select alien");
     }
 
   //If button is pressed then led is turned off
   } else if (mode == 2) {
     
+    mode2();
+    
+  
+  } else if (mode == 3) {
+
+    Serial.print("Tranformed into alien ");
+
+  }
+
+}
+
+void mode2() {
+
+  for(;mode == 2;) {
+
+    //Read select button state
+    selectbuttonState = digitalRead(SW);
+
+    //If select button is pressed then go to next mode
+    if (selectbuttonState == LOW) {
+      mode = 3;
+      Serial.println("Tranformed into alien");
+    }
+
+    //Read stasrt button state
+    buttonState = digitalRead(buttonPin);
+
+    // check if start button is pressed. If it is then go to the previous mode
     if (buttonState == HIGH) {
       // turn LED off:
       digitalWrite(LED, LOW);
       mode = 1;
       delay(200);
-      Serial.println("Button pressed");
+      Serial.println("Button pressed back to start");
     }
+
+
   }
 
 }
