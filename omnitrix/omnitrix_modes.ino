43ed6efccc3305939014317c59a1813e766ab7da
AA0100000101000001 <144900857+AA0100000101000001@users.noreply.github.com>
2023-12-27 13:47:20 +0200

    Move Switch Statements to omnitrix_modes

--- omnitrix/omnitrix_modes.ino
+++ omnitrix/omnitrix_modes.ino
@@ -1,4 +1,122 @@
 
+//Switch statement for start button modes
+void startButtonModes() {
+
+  switch (mode) {
+    //Omnitrix is in start mode
+    case 1:
+      //Go to select alien mode
+		  mode1to2();
+
+      //reset timer
+      start = RTC_getLocalEpoch();
+      break;
+
+    //Omnitrix is in select alien mode
+    case 2:
+      //Go to start mode
+      mode2to1();
+
+      //reset timer
+      start = RTC_getLocalEpoch();
+      break;
+  }
+
+}
+
+//Switch statement for right button modes
+void rightButtonModes() {
+
+  playSound(3); //Play move encoder forward 
+
+  switch (mode) {
+    //Omnitrix is in select alien mode
+    case 2:
+      //If the next alien number is bigger than the total number of aliens
+      //then go back to first alien, else go to the next alien
+      if ((alienNo  + 1)> ALIEN_NUMBER ) {
+        alienNo = 0;
+      }
+      else {
+        alienNo += 1;
+      }
+
+      //Display green backround png to erase previous alien
+      eraseAlien();
+      //Display alien
+      ShowAlien();
+
+      Serial.print("Right, alien no:");
+      Serial.println(alienNo);
+      delay(200);
+      break;
+  }
+
+  //reset timer
+  start = RTC_getLocalEpoch();
+
+}
+
+//Switch statement for left button modes
+void leftButtonModes() {
+
+  playSound(4); //Play move encoder backwards 
+
+  switch (mode) {
+    //Omnitrix is in select alien mode
+    case 2:
+      //If the next alien number is less than 0
+      //then go back to the last alien, else go to the previous alien
+      if ((alienNo - 1) < 0 ) {
+        alienNo = ALIEN_NUMBER;
+      }
+      else {
+        alienNo -= 1;
+      }
+          
+      //Erase previous alien
+      eraseAlien();
+      //Display alien
+      ShowAlien();
+
+      Serial.print("Left, alien no:");
+      Serial.println(alienNo);
+      delay(200);
+      break;
+  }
+
+  //reset timer
+  start = RTC_getLocalEpoch();
+
+}
+
+//Switch statement for select button modes
+void selectbuttonModes() {
+
+  switch (mode) {
+    //Omnitrix is in select alien mode
+    case 2:
+      //Go to transformation mode
+      mode2to3();
+
+      break;
+
+    //Omnitrix is in transformation mode
+    case 3:
+      //Go to Start mode
+      mode3to1();
+
+      break;
+  }
+
+  //reset timer
+  start = RTC_getLocalEpoch();
+
+  Serial.print("mode ");
+  Serial.println(mode);
+
+}
+
 //Mode 3
 //Function for transformation
 void transformedMode() {
