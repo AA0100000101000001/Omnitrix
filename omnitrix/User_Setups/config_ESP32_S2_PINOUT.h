3c891246b11dc6540487ea3a3d2acbb325d293c9
AA0100000101000001 <gracedgold26@gmail.com>
2024-04-26 23:40:00 +0300

    Add Configuration For Rotary Encoder

--- omnitrix/User_Setups/config_ESP32_S2_PINOUT.h
+++ omnitrix/User_Setups/config_ESP32_S2_PINOUT.h
@@ -12,8 +12,10 @@
 
 //Use one of these options for rotary encoder
 //1. Uncomment for use of micro switches for encoder
-#define MICRO_SWITCHES_ROTARY_ENCODER_ENABLED
+//#define MICRO_SWITCHES_ROTARY_ENCODER_ENABLED
 //2. Uncomment for use of magnetic encoder
+#define ROTARY_ENCODER_ENABLED
+//3. Uncomment for use of magnetic encoder
 //#define MAGNETIC_ROTARY_ENCODER_ENABLED
 
 //Define rotary encoder input pins
